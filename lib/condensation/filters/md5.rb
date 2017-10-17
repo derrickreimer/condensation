9c03d7776e5ede7e51070a77c175ad24474ed70a
Robyn-Dale Samuda <robyn@yuraki.com>
2017-10-17 11:31:50 -0500

    MD5 method

--- lib/condensation/filters/md5.rb
+++ lib/condensation/filters/md5.rb
@@ -1,8 +1,9 @@
 module Condensation
   module Filters
     module MD5
-      def md5
-
+      def md5(input)
+        return if input.nil?
+        Digest::MD5.hexdigest(input)
       end
     end
   end
